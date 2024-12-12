3ae0d0d44090a160cf663f3ad7acf7fc20d9c72c
Oussamahamani <oussamahamani2000@gmail.com>
2024-12-12 11:38:48 -0500

    adding intial code

---
+++ script.js
@@ -0,0 +1,10 @@
+const cities = [
+    { name: "Boston", temperature: 71 },
+    { name: "New York", temperature: 75 },
+    { name: "Denver", temperature: 45 },
+    { name: "Los Angeles", temperature: 80 },
+    { name: "Chicago", temperature: 65 },
+    { name: "Houston", temperature: 90 },
+];
+
+
