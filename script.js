058e080aaae43bcaffb58359b5e01bfa10890ec7
Oussamahamani <oussamahamani2000@gmail.com>
2024-12-12 11:59:43 -0500

    rendering cities

--- script.js
+++ script.js
@@ -8,3 +8,33 @@ const cities = [
 ];
 
 
+
+let ul = document.querySelector("#cityList")
+
+for (let city of cities){
+    // console.log(city)
+    let cityName = city.name
+    let temp = city.temperature
+
+    // ul.textContent= ul.textContent+  cityName  + " temperature is "+temp
+
+    // ul.innerHTML = ul.innerHTML+`<li>${cityName}</li>
+    // <li>Temperature: ${temp}</li>
+    // `
+
+    let cityLi = document.createElement("li")
+    cityLi.textContent = cityName
+    if(cityName === "Boston"){
+        cityLi.style.fontSize = "32px"
+    }
+    ul.append(cityLi)
+
+    let tempLi = document.createElement("li")
+    tempLi.textContent = `Temperature: ${temp}`
+    ul.append(tempLi)
+
+
+}
+
+//part 2: convert those to celsius 
+
